205d127ec95f6e1359c9720c86774b92a2f44729
Juan Rozas <juanbr.rozas2005@gmail.com>
2026-04-08 22:13:15 -0300

    fix: corrigindo pipeline

--- fundamentos/variaveis.js
+++ fundamentos/variaveis.js
@@ -5,3 +5,42 @@
 
 console.clear()
 console.log('Estudo das variáveis em JavaScript')
+console.log('') 
+
+// a linha a baixo mostra uma string no console 
+console.log('String')
+
+// a linha a baixo cria uma variavel do tipo string e mostra seu tipo no console
+let nome = 'Juan'
+console.log(nome)
+console.log(typeof(nome))
+
+console.log('')
+
+console.log('variavel do tipo Number')
+let idade = 20
+console.log(idade)
+console.log(typeof(idade))
+
+console.log('')
+
+let peso = 75.5
+console.log(peso)
+console.log(typeof(peso))
+
+console.log('')
+
+let altura = 1.83
+console.log(altura)
+console.log(typeof(altura))
+
+console.log('____________________________')
+console.log(' -     Ficha do aluno     - ')
+console.log('____________________________')
+
+// alinha abaixo concatena (uniao) um texto com o conteudo da variavel nome e mostra no console
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Peso: ${peso}`)
+console.log(`Altura: ${altura}`)
+
